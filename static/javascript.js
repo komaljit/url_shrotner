51599d2205e01bea1cf8c12e6a9148dd5802213f
KOMALJIT SINGH <31124879+komaljit@users.noreply.github.com>
2017-12-21 19:20:20 -0800

    Update javascript.js

--- static/javascript.js
+++ static/javascript.js
@@ -1,21 +1,27 @@
 function activateShortner(){
     document.getElementById("asText").disabled = false;
-    
 }
 
 function shortenUrl(){
-    
 var value = $('.input1').val();
 console.log(value)
 $.ajax({
   type: 'POST',
-  url: "http://localhost:6100/get_short_url",
+  url: "http://34.230.73.181:6100/get_short_url",
   data: JSON.stringify(value),
   contentType: 'application/json',
   success: function(data){
-      console.log(data);
     // do something with the received data
     document.getElementById("kom").value = data;
+    document.getElementById("cbtn").style.display="block";
   }
 });
+}
+
+function copy(){
+     var short_Url  = document.getElementById("kom");
+     short_Url.select();
+     alert(short_Url.value)
+     document.execCommand("Copy");
+
 }
